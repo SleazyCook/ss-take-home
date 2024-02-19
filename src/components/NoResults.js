3da3598ea14321cd530cb9b615e6296d60c4f56a
andrewcook <andrewjcook93@gmail.com>
2024-02-18 21:41:41 -0600

    Fix: typo in NoResults component, add detail to readme

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -4,7 +4,7 @@ function NoResults({searchQuery}) {
 
     return (
         <div className='no-results'>
-            <div className='no-results__text'>Sorry, we couldn't any results for&nbsp;
+            <div className='no-results__text'>Sorry, we couldn't find any results for&nbsp;
                  
                 <span className='no-results__search-term'>"{searchQuery}"</span>
                 &nbsp;at this time. Please check again in the near future as new products are routinely added to our site. </div>
